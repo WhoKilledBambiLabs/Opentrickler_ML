217ac2c5f0abda63d7fa6b5d48607d843449e9a7
Ian <tomanyusers@gmail.com>
2026-07-13 14:53:05 +0200

    Upgraded SDK, Added AI Skills, Flash Memory Reset Tool
    1. Had to create a new dummy project Pico Project in VS Code to force the install of the new SDK
    2. Added some skills for anybody using any Agents From Opentricker Repo
    3. Added a tool to reset the flash memory.

--- configure_env.ps1
+++ configure_env.ps1
@@ -1,17 +1,17 @@
 # Add compiler to the path
-$env:Path = "${env:USERPROFILE}/.pico-sdk/toolchain/14_2_Rel1/bin;" + $env:Path
+$env:Path = "${env:USERPROFILE}/.pico-sdk/toolchain/15_2_Rel1/bin;" + $env:Path
 
 # Specify Ninja path
-$env:Path = "$env:USERPROFILE\.pico-sdk\ninja\v1.12.1;" + $env:Path
+$env:Path = "$env:USERPROFILE\.pico-sdk\ninja\v1.13.2;" + $env:Path
 
 # Specify CMake path
-$env:Path = "$env:USERPROFILE\.pico-sdk\cmake\v3.31.5\bin;" + $env:Path
+$env:Path = "$env:USERPROFILE\.pico-sdk\cmake\v4.3.4\bin;" + $env:Path
 
 # Specify picotool path
-$env:Path = "$env:USERPROFILE\.pico-sdk\picotool\2.1.1\picotool;" + $env:Path
+$env:Path = "$env:USERPROFILE\.pico-sdk\picotool\2.3.0\picotool;" + $env:Path
 
 # Specify pioasm path
-$env:Path = "$env:USERPROFILE\.pico-sdk\tools\2.1.1\pioasm;" + $env:Path
+$env:Path = "$env:USERPROFILE\.pico-sdk\tools\2.3.0\pioasm;" + $env:Path
 
 # Specify OpenOCD Path
 $OPENOCD_PATH = "$env:USERPROFILE\.pico-sdk\openocd\0.12.0+dev"
@@ -21,4 +21,4 @@ $env:Path = "$OPENOCD_PATH;" + $env:Path
 $env:OPENOCD_SCRIPTS = "$OPENOCD_PATH\scripts"
 
 # Specify PICO_TOOLCHAIN_PATH
-$env:PICO_TOOLCHAIN_PATH="$env:USERPROFILE\.pico-sdk\toolchain\14_2_Rel1"
+$env:PICO_TOOLCHAIN_PATH="$env:USERPROFILE\.pico-sdk\toolchain\15_2_Rel1"
